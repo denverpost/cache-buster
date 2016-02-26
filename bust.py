b14381a31f0d0a8bde4383eeee354732d3cd3bb9
freejoe76 <jmurphy@denverpost.com>
2016-02-25 20:22:37 -0500

    Write tests, fix init method

--- bust.py
+++ bust.py
@@ -9,11 +9,17 @@ class Bust:
     """ Send a request to invalidate the cache.
         """
 
-    def init(self):
+    def __init__(self):
+        """ Initialize the object.
+            >>> b = Bust()
+            """
         self.h = httplib2.Http('')
 
     def bust(self, url):
         """ Bust cache.
+            >>> b = Bust()
+            >>> b.bust('http://www.denverpost.com/')
+            True
             """
         url = url.strip()
         if 'http' not in url or len(url) > 1000:
@@ -27,16 +33,23 @@ class Bust:
         return True
 
 def main(args):
-    """
+    """ Example usage:
+        python bust.py http://www.denverpost.com/
+        >>> args = build_parser(['-v', 'http://www.denverpost.com/', 'urls/test'])
+        >>> main(args)
+        http://www.denverpost.com/
+        urls/test
         """
     b = Bust()
     for bust in args.busts[0]:
+        if 'verbose' in args and args.verbose:
+            print bust
         if 'http' in bust:
             b.bust(bust)
         else:
             fh = open(bust.strip())
             for line in fh:
-                print line
+                b.bust(line)
     
 def build_parser(args):
     """ This method allows us to test the args.
@@ -49,7 +62,8 @@ def build_parser(args):
                                      epilog='')
     parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true",
                         help="Run doctests, display more info.")
-    parser.add_argument("busts", action="append", nargs="*")
+    parser.add_argument("busts", action="append", nargs="*",
+                        help="Takes a space-separated list of URLs and / or files of URLs to bust.")
     args = parser.parse_args(args)
     return args
 
