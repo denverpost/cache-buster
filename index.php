8a4ce4a2a8a181ee433997e78be901026df45f14
freejoe76 <jmurphy@denverpost.com>
2016-02-26 16:33:19 -0500

    Allow the php to handle collections of URLs

--- index.php
+++ index.php
@@ -17,3 +17,14 @@ if ( isset($_GET['url']) ):
         if ( purge_cache($url) == 1 ) echo "success";
     endif;
 endif;
+
+// Handle purge requests from pre-defined collections of URLs
+if ( isset($_GET['urls']) ):
+    $filename = htmlspecialchars(substr($_GET['urls'], 0, 100));
+    $urls = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    if ( count($urls) > 0 ):
+        foreach ( $urls as $url ):
+            if ( purge_cache($url) == 1 ) echo "success";
+        endforeach;
+    endif;
+endif;
